e583b58b25a0b4f5b66d71b4ce2abcce11092f10
TvamevAgarwal <91375320+TvamevAgarwal@users.noreply.github.com>
2021-10-29 13:02:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -35,10 +35,10 @@ function preload(){
 
 function setup() {
 
-  createCanvas(600,200)
+  createCanvas(windowWidth,windowHeight)
   
   //create a trex sprite
-  trex = createSprite(50,160,20,50);
+  trex = createSprite(50,height-40,20,50);
   trex.addAnimation("running", trex_running);
   trex.addAnimation("colide",trex_collided);
   trex.scale = 0.5;
@@ -46,35 +46,35 @@ function setup() {
   trex.setCollider("rectangle",0,0,80,trex.height)
   
   //create a ground sprite
-  ground = createSprite(200,180,400,20);
+  ground = createSprite(width/2,height-20,width,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
  
   
   //creating invisible ground
-  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround = createSprite(200,height-10,width,10);
   invisibleGround.visible = false;
   
   cactusG = new Group();
   cloudG = new Group();
   
-  gameover = createSprite(300,100,10,10)
+  gameover = createSprite(width/2,height/2,10,10)
   gameover.addImage(gameoverI)
   gameover.scale = 0.8
   
-  restart = createSprite(300,115,10,10)
+  restart = createSprite(width/2,height/2,10,10)
   restart.addImage(restartI)
   restart.scale = 0.5
 }
 
 function draw() {
   //set background color
-  background(180);
+  background(0);
   
-  fill(0)
+  fill(200)
   textSize(15)
-  text("Score : "+score,450,20)
-  text("HiScore : "+HiScore,300,20)
+  text("Score : "+score,width-100,20)
+  text("HiScore : "+HiScore,width/2,20)
   
   if(score>HiScore){
     HiScore = score 
@@ -96,12 +96,13 @@ function draw() {
   trex.changeAnimation("running",trex_running);
     
    // jump when the space key is pressed
-  if(keyDown("space")&& trex.y >= 160) {
+  if((touches.length > 0 || keyDown("space")) && trex.y >= height-45) {
     trex.velocityY = -12;
     jump.play();
+    touches=[];
   }
     
-    trex.velocityY = trex.velocityY + 0.5
+    trex.velocityY = trex.velocityY + 0.47
     
     if (ground.x < 0){
     ground.x = ground.width/2;
@@ -129,8 +130,9 @@ function draw() {
    gameover.visible = true 
    restart.visible = true
     
-   if(mousePressedOver(restart)){
+   if(mousePressedOver(restart) || touches.length > 0 ){
      reset();
+     touches=[];
    }
   
       
@@ -151,13 +153,13 @@ function reset(){
 
 function spawnClouds(){
   if(frameCount%60===0){
-    cloud = createSprite(650,random(30,100),20,20)
+    cloud = createSprite(width,random(30,100),20,20)
     cloud.addImage(cloudImg)
     cloud.scale = 0.5
     cloud.velocityX = -4
     cloud.depth = trex.depth
     trex.depth = trex.depth + 1
-    cloud.lifetime = 160
+    cloud.lifetime = width/2
     cloudG.add(cloud);
 
     } 
@@ -166,9 +168,9 @@ function spawnClouds(){
 
 function spawnCactus(){
   if(frameCount%90===0){
-    cactus = createSprite(610,160,20,20)
+    cactus = createSprite(width,height-40,20,20)
     cactus.velocityX = -(4+(score/300));
-    cactus.lifetime = 160
+    cactus.lifetime = width/2
     cactus.scale = 0.6
     cactusG.add(cactus);
     
